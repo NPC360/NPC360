20cff0758481bc7fd30fbc2ef332a6c867f2d0b9
Devin Mancuso <devin.mancuso@F5AU01M.home.gateway>
2015-01-11 22:39:57 +1030

    First major commit for FED signup form design.
    This is a first draft, enough to move on with development. Need to
    investigate field masking, client and server validation and whether the
    material design buttons are the best choice. The CSS has all been
    mashed together, lots of overriding styles, but I will sort it properly
    when I move to bootstrap for responsive framework.

---
+++ wsgi/static/js/button.js
@@ -0,0 +1,29 @@
+;(function($, window, document, undefined) {
+
+  'use strict';
+
+  var $ripples = $('.ripples');
+
+  $ripples.on('click.Ripples', function(e) {
+
+    var $this = $(this);
+    var $offset = $this.parent().offset();
+    var $circle = $this.find('.ripples__circle');
+
+    var x = e.pageX - $offset.left;
+    var y = e.pageY - $offset.top;
+
+    $circle.css({
+      top: y + 'px',
+      left: x + 'px'
+    });
+
+    $this.addClass('is-active');
+
+  });
+
+  $ripples.on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(e) {
+    $(this).removeClass('is-active');
+  });
+
+})(jQuery, window, document);
