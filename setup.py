4373d9b4fd141817408d141bb064cbb1ff696701
Neal Shyam <neal@audioshocker.com>
2015-01-10 22:24:23 -0500

    update setup.py for dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='NPC360 SMSIO',
+setup(name='NPC360',
       version='1.0',
-      description='SMS IO controller for NPC360',
+      description='NPC360 engine',
       author='NPC360',
       author_email='NPC360@foo.bar',
       url='http://foo.bar',
-      install_requires=['Flask>=0.7.2', 'requests', 'twilio','datetime','logging','os', 'tinydb', 'sqlalchemy' ],
+      install_requires=['Flask>=0.7.2', 'requests', 'json', 'twilio','datetime','logging','os', 'sqlalchemy', 'random' ],
      )
