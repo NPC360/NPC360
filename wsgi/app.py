80ed218b8319fcd00f8e8f5afa6425da0e8057e1
Devin Mancuso <devin.mancuso@F5AU01M.home.gateway>
2015-04-01 22:45:55 +1030

    Completed signup HTML for IRL demo
    Broke the DB though, Neal need to add some more columns

--- wsgi/app.py
+++ wsgi/app.py
@@ -38,8 +38,7 @@ landing page / HTML / authorization routes
 def index():
     return render_template('index.html')
 
-#@app.route("/careers", methods = ['GET','POST'])
-@app.route("/signup", methods = ['GET','POST'])
+@app.route("/careers", methods = ['GET','POST'])
 def signup():
     if request.method == 'POST':
         # if auth code has been passed in, we need to process it.
