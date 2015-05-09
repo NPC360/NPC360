b4da55d060ba42cbd894469d3cae3c21eebf1844
Lachlan McDonald <lacmcdonald@deloitte.com.au>
2015-05-09 17:51:55 +1000

    Updated Compass config to properly handle the WSGI directory

--- config.rb
+++ config.rb
@@ -2,9 +2,13 @@ project_type    = :stand_alone
 environment     = :development
 http_path       = "/"
 sass_path       = "scss"
-css_dir         = "wsgi/static/css"
-images_dir      = "wsgi/static/img"
-javascripts_dir = "wsgi/static/js"
-fonts_dir       = "wsgi/static/fonts"
+css_path         = "wsgi/static/css"
+images_path      = "wsgi/static/img"
+javascripts_path = "wsgi/static/js"
+fonts_path       = "wsgi/static/fonts"
 output_style    = :compact
 relative_assets = false
+
+http_stylesheets_path = "/static/css"
+http_images_path      = "/static/img"
+http_javascripts_path = "/static/js"
