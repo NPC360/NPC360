46ca899a833290fb5b0d749dad6f828170caba72
Lachlan McDonald <lacmcdonald@deloitte.com.au>
2015-05-17 14:44:35 +1000

    Made a form with just the mobile number for the bad auth steps

--- wsgi/forms.py
+++ wsgi/forms.py
@@ -6,16 +6,27 @@ from wtforms.fields.html5 import TelField
 from wtforms.validators import StopValidation
 
 
-class Signup(Form):
-    def existing_email_check(self, field):
-        user = getUser(field.data)
-        if user is None:
-            raise StopValidation('Your email address is already in use.')
-
+class Phone(Form):
     def existing_mobile_check(self, field):
-        user = getUser(field.data)
+        # TODO: UNCOMMENT
+        # user = getUser(field.data)
+        user = True
         if user is None:
             raise StopValidation('Your mobile number is already in use.')
+
+    mobile_number = TelField('Mobile Number', [
+        validators.InputRequired(),
+        existing_mobile_check
+    ])
+
+
+class Signup(Phone):
+    def existing_email_check(self, field):
+        # TODO: UNCOMMENT
+        # user = getUser(field.data)
+        user = True
+        if user is None:
+            raise StopValidation('Your email address is already in use.')
 
     first_name = StringField('First Name', [
         validators.InputRequired(),
@@ -29,10 +40,6 @@ class Signup(Form):
         validators.InputRequired(),
         validators.Email(),
         existing_email_check
-    ])
-    mobile_number = TelField('Mobile Number', [
-        validators.InputRequired(),
-        existing_mobile_check
     ])
     tz = HiddenField(id="tz", [])
 
