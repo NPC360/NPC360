ac64d2d2a09eaff7f0df9e7ef64089aacf12703f
Neal Shyam <neal@audioshocker.com>
2015-06-05 22:59:18 -0400

    ok, let's just reply on local logfile?

--- wsgi/logstuff.py
+++ wsgi/logstuff.py
@@ -4,6 +4,7 @@ SETUP BASIC LOGGING
 # papertrail stuff from http://help.papertrailapp.com/kb/configuration/configuring-centralized-logging-from-python-apps/
 import logging
 import socket
+from os import environ
 import logging.handlers
 
 #from logging.handlers import SysLogHandler
@@ -20,14 +21,14 @@ log.setLevel(logging.DEBUG)
 lf = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', datefmt='%Y-%m-%dT%H:%M:%S')
 
 # paper trail handler
-ptcf = ContextFilter()
-log.addFilter(ptcf)
-pt = logging.handlers.SysLogHandler(address=('logs2.papertrailapp.com', 18620))
-pt.setFormatter(lf)
-log.addHandler(pt)
+#ptcf = ContextFilter()
+#log.addFilter(ptcf)
+#pt = logging.handlers.SysLogHandler(address=('logs2.papertrailapp.com', 18620))
+#pt.setFormatter(lf)
+#log.addHandler(pt)
 
 # file handler
-#fh = logging.FileHandler('log/log.txt')
+#fh = logging.FileHandler('log/npc360.log')
 #fh.setLevel(logging.DEBUG)
 #fh.setFormatter(lf)
 #log.addHandler(fh)
