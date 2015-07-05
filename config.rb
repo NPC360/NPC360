3e6a3432af7530014cf4d8bb50b3d7492fd786ab
Lachlan McDonald <lacmcdonald@deloitte.com.au>
2015-07-05 19:33:00 +1000

    Whitespace

--- config.rb
+++ config.rb
@@ -1,15 +1,15 @@
-project_type    = :stand_alone
-environment     = :development
-http_path       = "/"
-sass_path       = "scss"
-css_path         = "wsgi/static/css"
-images_path      = "wsgi/static/img"
+project_type = :stand_alone
+environment = :development
+http_path = "/"
+sass_path = "scss"
+css_path = "wsgi/static/css"
+images_path = "wsgi/static/img"
 javascripts_path = "wsgi/static/js"
-fonts_path       = "wsgi/static/fonts"
+fonts_path = "wsgi/static/fonts"
 line_comments = false
-output_style    = :compact
+output_style = :compact
 relative_assets = false
 
 http_stylesheets_path = "/static/css"
-http_images_path      = "/static/img"
+http_images_path = "/static/img"
 http_javascripts_path = "/static/js"
